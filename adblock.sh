0421bc632cec5e56862f20fe0aa8e2c43d0db764
Stephen Damm <shinhalsafar@gmail.com>
2014-06-10 15:07:44 -0600

    Update adblock.sh
    - updated with newer version I've been using
    - now gathers all ads at once and parses at once
    - has example section for white listing, will expand later
    - stores the downloaded hosts file in /tmp
    - config section at top

--- adblock.sh
+++ adblock.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
+#
+# Simple AdBlock Script
+#
+
+ADBLOCK_HOSTS="/tmp/hosts.clean"
+DNS_CONF_FILE="/jffs/dnsmasq.adblock.conf"
+DNS_CONF_LINE="addn-hosts=$ADBLOCK_HOSTS";
+ROUTER_IP="192.168.1.106"
+
 # Down the DNSmasq formatted ad block list
-curl "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro=0&mimetype=plaintext" | sed "s/127\.0\.0\.1/127\.0\.0\.1#8888/">/jffs/dnsmasq.adblock.conf
+echo "Downloading AdBlock files... Parsing for dnsmasq..."
+wget -qO- "http://winhelp2002.mvps.org/hosts.txt" "http://someonewhocares.org/hosts/zero/hosts" "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate[day]=&startdate[month]=&startdate[year]=&mimetype=plaintext&useip=0.0.0$
+     grep -w ^0.0.0.0 |
+     sed $'s/\r$//;s/0.0.0.0/192.168.1.106/' |
+     sed $'s:#.*$::g' |
+     sort -u > $ADBLOCK_HOSTS
 
+# remove entries from list - hacked in whitelist for now
+echo "Whitelisting entries..."
+sed -i '/.*admob.*/d' $ADBLOCK_HOSTS
 
-# Download another list, manipulate and echo to host file instead, easier
-wget -O - http://www.mvps.org/winhelp2002/hosts.txt |
-        grep 127.0.0.1 |
-        sed '2,$s/127.0.0.1/0.0.0.0/g; s/[[:space:]]*#.*$//g;' |
-        grep -v localhost |
-        tr ' ' '\t' |
-        tr -s '\t' |
-        tr -d '\015' |
-        sort -u >/tmp/hosts0
+# add line to dnsmasq config file
+echo "addn-hosts=$ADBLOCK_HOSTS" > $DNS_CONF_FILE
 
-echo "addn-hosts=/tmp/hosts0" >> /jffs/dnsmasq.adblock.conf
-
-
-# Reboot dnsmasq, important, lose internet if this fails
+# Reboot dnsmasq, important, cannot use dns locally if this fails
+echo "Rebooting dnsmasq..."
 killall dnsmasq
 dnsmasq -c 1500 --log-async -n
-
-
